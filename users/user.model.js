68028467c62bb51718dc0c57fe2719753325ff64
Anant Kumar Iyer <42653125+anantiyer896@users.noreply.github.com>
2022-09-30 13:10:05 +0530

    initial commit

---
+++ users/user.model.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ROLE = {
+    USER: 'user',
+    AUDITOR: 'auditor'
+}
+
+const schema = new Schema({
+    username: { type: String, unique: true, required: true },
+    hash: { type: String, required: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    createdDate: { type: Date, default: Date.now },
+    role: {type: String, required: true}
+});
+
+schema.set('toJSON', { virtuals: true });
+
+module.exports = mongoose.model('User', schema);
+module.exports = {ROLE: ROLE} 
